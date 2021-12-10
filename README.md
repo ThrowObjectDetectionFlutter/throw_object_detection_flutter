d556af408443c72f230d8e6eac1e1fa3af806a05
EndureBlaze <nihaocun@163.com>
2021-12-10 20:14:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# throw_object_detection_flutter
+
+A new Flutter project.
+
+## 环境
+
+```shell
+-> flutter --version
+Flutter 2.6.0-12.0.pre.978 • channel master • git@github.com:flutter/flutter.git
+Framework • revision f9ff3204f9 (22 hours ago) • 2021-12-09 09:29:11 -0500
+Engine • revision 59778ae91c
+Tools • Dart 2.16.0 (build 2.16.0-85.0.dev) • DevTools 2.9.1
+```
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? file;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+              if (result != null) {
+                setState(() {
+                  file = File(result.files.single.path ?? "");
+                });
+              } else {}
+            },
+            child: const Text("点我选文件"),
+          ),
+          file == null ? Container() : Image.file(file!)
+        ],
+      )),
+    );
+  }
+}
